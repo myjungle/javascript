7b6c569be633ced1467694d95b3cc464944b51b0
myjungle <comaflying@naver.com>
2014-05-29 23:45:47 +0900

    Create fixedcontrol.js

---
+++ fixedcontrol.js
@@ -0,0 +1,59 @@
+;(function($, core, ui, undefined){
+   "use strict";
+   
+  	ui('FixedBottom', {
+		bindjQuery: 'fixedBottom',
+		defaults: {
+			minWidth: 1000,
+			offsetTarget: ''
+		},
+		initialize: function(el, options) {
+			var me = this;
+			if(me.callParent(el, options) === false){ return; }
+
+			if(me.options.offsetTarget){
+				me.$offsetTarget = $(me.options.offsetTarget);
+			}
+
+			me.winHeight = emart.util.getWinHeight();
+			me.winWidth = emart.util.getWinWidth();
+			me.scrollTop = emart.$win.scrollTop();
+			me.scrollLeft = emart.$win.scrollLeft();
+			me._bindEvents();
+			me._reposition();
+		},
+		_bindEvents: function() {
+			var me = this,
+				$win = emart.$win,
+				util = emart.util;
+
+			$win.on('resize.'+me.cid, function() {
+				me.winHeight = $win.innerHeight(); //util.getWinHeight();
+				me.winWidth = $win.innerWidth(); //util.getWinWidth();
+				me._reposition();
+			}).on('scroll.'+me.cid, function() {				
+				me.scrollTop = $win.scrollTop();
+				me.scrollLeft = $win.scrollLeft();
+				me._reposition();
+			}).triggerHandler('resize.'+me.cid);
+		},
+
+		_reposition: function() {
+			var me = this,
+				$win = emart.$win,
+				util = emart.util,
+				opts = me.options,
+
+				docHeight = util.getDocHeight(),
+				scrollHeight = me.scrollTop + me.winHeight, //docHeight - footerHeight,
+				contentHeight = me.$offsetTarget ? me.$offsetTarget.offset().top : docHeight;
+
+			me.$el.css({
+				left: -me.scrollLeft,
+				bottom: contentHeight >= scrollHeight ? 0 :  (scrollHeight - contentHeight),
+				width: Math.max(opts.minWidth, me.winWidth)
+			});
+		}
+	});
+
+})(jQuery, window[LIB_NAME], window[LIB_NAME].ui); 
